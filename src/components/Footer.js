c0f0da1ff9363983125c953c7c122df53139bc9d
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-05-09 17:16:30 +0530

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,17 +13,15 @@ function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
+        
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/BhathiyaPrasad"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -33,7 +31,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://twitter.com/bhathiya_prasad"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -43,7 +41,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/bhathiya-prasad-30a3b32ab/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +51,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/soumyajit4419"
+                href="https://www.instagram.com/bhathiya.prasad.3/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
