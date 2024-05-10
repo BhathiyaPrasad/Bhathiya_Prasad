0d1f105ffc7742a0e61c6b15c91b79c97bf9c161
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-05-10 09:09:43 +0530

    added projects

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Bhathiya Prasad </span>
+            from <span className="purple"> Gampaha, Sri Lanka.</span>
             <br />
             I am currently Undergraduate Student from University Of Colombo School of Computing.
             <br />
