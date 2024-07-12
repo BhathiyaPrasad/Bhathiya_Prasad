929faec412e672257cf9726c9e28ea7c5c69fe57
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-12 11:30:38 +0530

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+import soulfood from "../../Assets/Projects/soulfood.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import sweetbeans from "../../Assets/Projects/sweetbeans.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import portfolio from "../../Assets/Projects/portfolio.png";
 
 function Projects() {
   return (
@@ -26,18 +26,18 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="E Commerce Clothing Store"
-              description="A Ecommerce Website For Clothing Store build And Hosted Using   HTML, CSS, BOOTSTRAP, JAVASCRIPT, NODE, FIREBASE ."
-              ghLink="https://github.com/soumyajit4419/Chatify"
+              description=""
+              ghLink="https://Salunifashion.lk/"
               demoLink="https://Salunifashion.lk/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={portfolio}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="Welcome to my portfolio, built with Next.js, React, and Tailwind CSS for a modern, fast, and responsive experience."
+              title="Personal Portfolio"
+              description=""
               ghLink="https://github.com/BhathiyaPrasad/Bhathiya_Prasad.git"
               demoLink="https://bhathiyaprasad.com"
             />
@@ -45,23 +45,23 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={sweetbeans}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Sweet Beans Cafe Website"
+              description=""
+              ghLink="https://sweetbeans.lk"
+              demoLink="https://sweetbeans.lk"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={soulfood}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Restaurant QR Food Order With Website - SoulFoods"
+              description=""
+              ghLink="https://soulfood.lk"
+              demoLink="https://soulfood.lk"
             />
           </Col>
 
@@ -70,9 +70,9 @@ function Projects() {
               imgPath={suicide}
               isBlog={false}
               title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              description=""
+              ghLink=""
+              demoLink="" 
             />
           </Col>
 
@@ -81,10 +81,9 @@ function Projects() {
               imgPath={emotion}
               isBlog={false}
               title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              description=""
+              ghLink=""
+              demoLink=""    
             />
           </Col>
         </Row>
