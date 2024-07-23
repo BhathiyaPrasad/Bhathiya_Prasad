ff163a1f4ff91e9f19c901884e9fe5331b41b507
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-07-23 23:56:34 +0530

    update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Bhathiya_Prasad.pdf";
+import pdf from "../../Assets/../Assets/Bhathiya_prasad.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
